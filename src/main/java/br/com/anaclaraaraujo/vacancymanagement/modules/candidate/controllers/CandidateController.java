a59cb97077ba027b3028eb95f332d807a59b13a4
anaclaraaraujo <helloanaclaraaraujo@gmail.com>
2025-03-14 21:06:13 -0300

    feat: add Swagger documentation in Controller and implement UseCase for listing jobs by filter

--- src/main/java/br/com/anaclaraaraujo/vacancymanagement/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/anaclaraaraujo/vacancymanagement/modules/candidate/controllers/CandidateController.java
@@ -1,15 +1,21 @@
 package br.com.anaclaraaraujo.vacancymanagement.modules.candidate.controllers;
 
+import java.util.List;
 import java.util.UUID;
 
+import br.com.anaclaraaraujo.vacancymanagement.modules.candidate.useCases.ListAllJobsByFilterUseCase;
+import br.com.anaclaraaraujo.vacancymanagement.modules.company.entities.JobEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.anaclaraaraujo.vacancymanagement.modules.candidate.CandidateEntity;
 import br.com.anaclaraaraujo.vacancymanagement.modules.candidate.useCases.CreateCandidateUseCase;
@@ -25,6 +31,9 @@ public class CandidateController {
 
     @Autowired
     private ProfileCandidateUseCase profileCandidateUseCase;
+
+    @Autowired
+    private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
 
     @PostMapping("/")
     public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
@@ -48,5 +57,19 @@ public class CandidateController {
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+
+    @GetMapping("/job")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    @Tag(name = "Candidato", description = "Informações do candidato")
+    @Operation(summary = "Listagem de vagas disponível para o candidato", description = "Essa função é responsável por listar todas as vagas disponíveis, baseada no filtro")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(array = @ArraySchema(schema = @Schema(implementation = JobEntity.class)))
+            })
+    })
+    public List<JobEntity> findJobByFilter(@RequestParam String filter) {
+        return this.listAllJobsByFilterUseCase.execute(filter);
     }
 }
